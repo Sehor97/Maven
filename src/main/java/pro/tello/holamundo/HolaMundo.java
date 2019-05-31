6aeb3d773973a57e96a054febcab4d08bc874f9a
Sehor97 <sebas199765@gmail.com>
2019-05-31 10:14:59 -0500

    commit 4

--- src/main/java/pro/tello/holamundo/HolaMundo.java
+++ src/main/java/pro/tello/holamundo/HolaMundo.java
@@ -2,7 +2,7 @@ package pro.tello.holamundo;
 
 public class HolaMundo {
 	
-	public static final String SALUDO = "Hola, Mundo222!";
+	public static final String SALUDO = "Hola, Mundo222!, Ingenieria";
 
 	public String saludar() {
 		return HolaMundo.SALUDO;
